d8c0d936530015317eef1521520be24aec8d18cd
Brock Shaffer <109301662+CodedMasonry@users.noreply.github.com>
2026-02-03 09:25:43 -0500

    implement core window buttons

--- symphony-desktop/src/components/top-bar.tsx
+++ symphony-desktop/src/components/top-bar.tsx
@@ -7,20 +7,30 @@ import {
   MenubarSeparator,
   MenubarTrigger,
 } from "@/components/ui/menubar";
-
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+
 import {
   Close,
+  Expand,
   HelpCircleIcon,
   Logout,
+  Maximize,
+  Maximize01Icon,
+  Minimize,
+  Minus,
   SettingsIcon,
 } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
+import { getCurrentWindow } from "@tauri-apps/api/src/window";
 
 function TopBar({ className, ...props }: React.ComponentProps<"div">) {
+  const appWindow = getCurrentWindow();
+
   return (
     <div
-      className={cn("flex align-middle px-1 border-b border-border", className)}
+      data-tauri-drag-region
+      className={cn("flex align-middle px-2 border-b border-border", className)}
       {...props}
     >
       <Menubar className="border-0">
@@ -45,8 +55,28 @@ function TopBar({ className, ...props }: React.ComponentProps<"div">) {
           </MenubarContent>
         </MenubarMenu>
       </Menubar>
-      <div className="flex ml-auto my-auto">
-        <HugeiconsIcon icon={Close} strokeWidth={2} />
+      <div className="flex ml-auto my-auto space-x-2">
+        <Button
+          variant="ghost"
+          size="icon-xs"
+          onClick={() => appWindow.toggleMaximize()}
+        >
+          <HugeiconsIcon icon={Expand} strokeWidth={4} />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon-xs"
+          onClick={() => appWindow.minimize()}
+        >
+          <HugeiconsIcon icon={Minus} strokeWidth={4} />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon-xs"
+          onClick={() => appWindow.close()}
+        >
+          <HugeiconsIcon icon={Close} strokeWidth={4} />
+        </Button>
       </div>
     </div>
   );
